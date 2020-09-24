058d84228cf1f98cc98d07757525e9ddfaab0abb
dalibaffoun <mabaffoun@gmail.com>
2020-09-24 22:07:35 +0100

    ferid m3alem

--- src/alfa/Application.java
+++ src/alfa/Application.java
@@ -3,5 +3,6 @@ public class Application {
 
     public static void main(String[] argv){
         System.out.print("Hello word !!!!!!");
+        System.out.print("Farid ya ma3lemmm !!!!!!");
     }
 }
